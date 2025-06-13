b824d0ecd4146f038dfa69438e967e5ccdc869e7
trannhon70 <kevintran351996@gmail.com>
2025-06-13 14:32:32 +0700

    update

--- index.php
+++ index.php
@@ -40,14 +40,55 @@
                         <img loading="lazy" width="25px" height="25px" src="<?php echo $local ?>/images/icons/icon_heat.webp" alt="...">
                     </div>
                     <div class="section1__bottom_right">
-                        <div class="section1__bottom_right_item" >
+                        <div class="section1__bottom_right_item">
                             <img loading="lazy" width="25px" height="25px" src="<?php echo $local ?>/images/icons/icon_prev.webp" alt="...">
                         </div>
-                        <div class="section1__bottom_right_item" >
+                        <div class="section1__bottom_right_item">
                             <img loading="lazy" width="25px" height="25px" src="<?php echo $local ?>/images/icons/icon_next.webp" alt="...">
                         </div>
                     </div>
                 </div>
+            </section>
+
+            <section class="section2">
+                <img loading="lazy" width="100%" height="100%" src="<?php echo $local ?>/images/banner/banner_1.webp" alt="...">
+            </section>
+
+            <section class="section3">
+                <div class="section3__body">
+                    <div class="section3__body_left">
+                        <div class="section3__body_left_title">
+                            CÁC HẠNG MỤC KHÁM CHỮA
+                        </div>
+                        <div class="section3__body_left_list ">
+                            <div class="section3__body_left_list_item">
+                                <img loading="lazy" width="90px" height="90px" src="<?php echo $local ?>/images/icons/icon_BXH.webp" alt="...">
+                                <div class="section3__body_left_list_item_text">BỆNH XÃ HỘI</div>
+                            </div>
+
+                            <div class="section3__body_left_list_item">
+                                <img loading="lazy" width="90px" height="90px" src="<?php echo $local ?>/images/icons/icon_BDL.webp" alt="...">
+                                <div class="section3__body_left_list_item_text">BỆNH DA LIỄU</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section3__body_right">
+                        <img loading="lazy" width="100%" height="100%" src="<?php echo $local ?>/images/banner/section3_bacsi.webp" alt="...">
+                    </div>
+                </div>
+            </section>
+
+            <section class="section4">
+                <div class="section4__title">hệ thống cơ sở vật chất tại phòng khám</div>
+                <div class="section4__list">
+                    <div class="section4__list_item">
+                        <img loading="lazy" width="100%" height="100%" src="<?php echo $local ?>/images/banner/section4_left.webp" alt="...">
+                    </div>
+                    <div class="section4__list_item">
+                        <img loading="lazy" width="100%" height="100%" src="<?php echo $local ?>/images/banner/section4_right.webp" alt="...">
+                    </div>
+                </div>
+                <img loading="lazy" width="100%" height="100%" src="<?php echo $local ?>/images/banner/section4_chat_luong.webp" alt="...">
             </section>
         </article>
     </main>
