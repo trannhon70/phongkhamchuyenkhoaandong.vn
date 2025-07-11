ee69c73aa36ff5f743f599da193100f9735f65a2
trannhon70 <kevintran351996@gmail.com>
2025-07-11 16:56:52 +0700

    update

---
+++ khuyen-mai.php
@@ -0,0 +1,322 @@
+<?php include_once "inc/header.php" ?>
+<meta name="description"
+    content="Phòng khám chuyên điều trị bệnh uy tính tại thành phố Hồ Chí Minh">
+<title>Phòng khám chuyên khoa</title>
+<link rel="stylesheet" href="css/khuyen-mai.min.css">
+</head>
+
+<body>
+    <?php include_once "./layout/header_layout.php" ?>
+    <main style="transform: translate(0px, -10px);">
+        <article>
+            <section id="endow" class="endow">
+                <div class="endow__container">
+                    <div class="endow__container-top">
+                        Khám sức khỏe sinh sản không chỉ là quan tâm sức khỏe của bản thân mà còn thể hiện trách nhiệm với bạn đời, với hạnh phúc gia đình và sức khỏe của con cái trong tương lai.
+                        <br><br>
+
+                        Theo khuyến cáo của các chuyên gia y tế, nam giới độ tuổi sinh sản nên đi khám định kỳ tối thiểu 6 tháng 1 lần, đặc biệt là trước khi sinh con. Việc chủ quan, phát hiện bệnh khi đã muộn có thể dẫn tới những biến chứng đáng tiếc.
+                        <br><br>
+
+                        Thấu hiểu tầm quan trọng của việc khám sức khỏe sinh sản nam giới, Phòng khám chuyên khoa An Đông đã triển khai chương trình ưu đãi “Sức Khỏe Như Ý – Gói Khám Hợp Lý” có đầy đủ các danh mục cần thiết và phí thăm khám được điều chỉnh phù hợp với nhiều đối tượng.
+                    </div>
+                    <div class="endow__container-img">
+                        <img loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/icon_uudai.webp" alt="...">
+                    </div>
+                    <div class="endow__container-rowPC">
+                        <div class="endow__container-row ">
+                            <div class="endow__container-row-left">
+                                <img loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/goi_kham.webp" alt="...">
+                            </div>
+                           
+                            <div class="endow__container-row-left">
+                                <img loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/goi_kham2.webp" alt="...">
+                            </div>
+                        </div>
+                        <div class="endow__container-pagi">
+                            <span class="endow__container-pagi-item"></span>
+                            <span class="endow__container-pagi-item endow__container-pagi-item-active"></span>
+                            <span class="endow__container-pagi-item"></span>
+                            <span class="endow__container-pagi-item"></span>
+                        </div>
+                    </div>
+                    <div class="endow__container-rowMB">
+                        <div class="endow__container-row ">
+                            <div class="endow__container-row-item endow__container-row-item-active">
+                                <img  width="270px" src="<?php echo $local ?>/images/khuyen_mai/goi_kham.webp" alt="...">
+                            </div>
+                            <!-- <div class="endow__container-row-item ">
+                                <img  width="270px" src="<?php echo $local ?>/images/khuyen_mai/goi_kham1.webp" alt="...">
+                            </div> -->
+                            <div class="endow__container-row-item ">
+                                <img  width="270px" src="<?php echo $local ?>/images/khuyen_mai/goi_kham2.webp" alt="...">
+                            </div>
+                        </div>
+                        <div class="endow__container-pagi">
+                            <span class="endow__container-pagi-itemMB endow__container-pagi-itemMB-active"></span>
+                            <span class="endow__container-pagi-itemMB"></span>
+                            <span class="endow__container-pagi-itemMB"></span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="endow_1">
+            </section>
+            <section class="method" >
+                <div class="method__container" >
+                    <img class="method__container-img" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/phuong_phap.webp" alt="...">
+                    <div class="method__container-list" >
+                        <div class="method__container-list-item method__container-list-item-active" >
+                            <img  width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/phuong_phap_1.webp" alt="...">
+                        </div>
+                        <div class="method__container-list-item " >
+                            <img  width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/phuong_phap_2.webp" alt="...">
+                        </div>
+                        <div class="method__container-list-item " >
+                            <img  width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/phuong_phap_3.webp" alt="...">
+                        </div>
+                    </div>
+                    <div class="method__container-pagi" >
+                        <div class="method__container-pagi-item method__container-pagi-item-active" ></div>
+                        <div class="method__container-pagi-item" ></div>
+                        <div class="method__container-pagi-item" ></div>
+                    </div>
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/bang_chi_phi.webp" alt="...">
+                </div>
+            </section>
+            <section class="team">
+                <div class="team__container">
+                   <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/giay_phep_hoat_dong.webp" alt="...">
+                  
+                </div>
+            </section>
+            <section class="teamMB">
+                 <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/giay_phep_hoat_dong.webp" alt="...">
+
+            </section>
+            <section class="clinic">
+                <img class="clinic__image" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/bg_phong_kham.webp" alt="...">
+                <div class="clinic__container">
+                    <div class="clinic__container-body">
+                        <img loading="lazy" width="870px" height="260px" src="<?php echo $local ?>/images/khuyen_mai/phong_kham.webp" alt="...">
+
+                    </div>
+
+                </div>
+                <a href="https://npa.zoosnet.net/LR/Chatpre.aspx?id=NPA46777247&lng=en" class="clinic__container-bottom">
+                    <img loading="lazy" width="auto" height="auto" src="<?php echo $local ?>/images/khuyen_mai/button_lien_he.webp" alt="...">
+                </a>
+            </section>
+            <section class="clinicMB">
+                <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/phong_kham.webp" alt="...">
+                <div>
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_location.webp" alt="...">
+                </div>
+                <div class="clinicMB_flex">
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_doi_ngu.webp" alt="...">
+                </div>
+                <div class="clinicMB_flex">
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_co_so.webp" alt="...">
+                </div>
+                <div class="clinicMB_flex">
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_may_moc.webp" alt="...">
+                </div>
+                <div class="clinicMB_flex">
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_dich_vu.webp" alt="...">
+                </div>
+                <div class="clinicMB_flex">
+                    <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/img_time.webp" alt="...">
+                </div>
+                <a href="https://npa.zoosnet.net/LR/Chatpre.aspx?id=NPA46777247&lng=en" class="clinicMB_flex">
+                    <img loading="lazy" width="70%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/button_lien_he.webp" alt="...">
+                </a>
+            </section>
+            <section class="consultation">
+                <div class="consultation__container">
+                    <img class="consultation__container-img" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/tu_van_online.webp" alt="...">
+                    <div class="consultation__container-row">
+                        <div class="consultation__container-row-left">
+                            <div id="form" class="consultation__container-row-left-body">
+                                <div class="consultation__container-row-left-body-title">Chúng tôi cam kết bảo mật 100% thông tin của quý khách <br>
+                                    Tuyệt đối không cung cấp cho bên thứ 3!</div>
+                                <div class="consultation__container-row-left-body-input">
+                                    <input type="text" placeholder="Họ tên">
+                                </div>
+                                <div class="consultation__container-row-left-body-input">
+                                    <input type="number" placeholder="Số điện thoại">
+                                </div>
+                                <a href="https://npa.zoosnet.net/LR/Chatpre.aspx?id=NPA46777247&lng=en" class="consultation__container-row-left-body-button">
+                                    <img loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/button_dang_ky.webp" alt="...">
+</a>
+                            </div>
+                        </div>
+                        <div class="consultation__container-row-right">
+                            <img class="consultation__container-img" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/tu_van_1.webp" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="consultationMB">
+                <img width="100%" height="auto" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/user_tuvan.webp" alt="...">
+                <div class="consultationMB__img">
+                    <img width="100%" height="auto" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/img-camket.webp" alt="...">
+                </div>
+                <div>
+                    <img width="100%" height="auto" loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/img_uudai.webp" alt="...">
+                </div>
+                <div id="form1" class="consultation__container-row-left-body">
+                  
+                    <a href="https://npa.zoosnet.net/LR/Chatpre.aspx?id=NPA46777247&lng=en" class="consultation__container-row-left-body-button">
+                        <img loading="lazy" src="<?php echo $local ?>/images/khuyen_mai/button_dang_ky.webp" alt="...">
+                    </a>
+                </div>
+                <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/tu_van_1.webp" alt="...">
+            </section>
+            
+            <section class="map">
+                <img loading="lazy" width="100%" height="auto" src="<?php echo $local ?>/images/khuyen_mai/map.webp" alt="...">
+                <div class="map__container">
+                    <div class="map__container-body">
+                        <a href="#">
+                            <img loading="lazy" width="252px" height="auto" src="<?php echo $local ?>/images/khuyen_mai/icon_dang_ky.webp" alt="...">
+                        </a>
+                        <a href="#">
+                            <img loading="lazy" width="252px" height="auto" src="<?php echo $local ?>/images/khuyen_mai/icon_tu_van.webp" alt="...">
+                        </a>
+                        <a href="#">
+                            <img loading="lazy" width="252px" height="auto" src="<?php echo $local ?>/images/khuyen_mai/icon_zalo.webp" alt="...">
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </article>
+    </main>
+
+    <script defer >
+         const itemsMethod = document.querySelectorAll('.method__container-list-item')
+         const pagiMethodItems = document.querySelectorAll('.method__container-pagi-item');
+         let currentMethod = 0
+
+         function changeSlide1(index) {
+            itemsMethod[currentMethod].classList.remove('method__container-list-item-active');
+            pagiMethodItems[currentMethod].classList.remove('method__container-pagi-item-active');
+            currentMethod = index;
+            itemsMethod[currentMethod].classList.add('method__container-list-item-active');
+            pagiMethodItems[currentMethod].classList.add('method__container-pagi-item-active');
+        }
+
+
+        pagiMethodItems.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                changeSlide1(index);
+            });
+        });
+
+        setInterval(() => {
+            let nextIndex = (currentMethod + 1) % itemsMethod.length;
+            changeSlide1(nextIndex);
+        }, 5000);
+    </script>
+
+    <script defer >
+        const itemsCus = document.querySelectorAll('.endow__container-row-item')
+        const pagiItems = document.querySelectorAll('.endow__container-pagi-itemMB');
+        let current = 0
+
+        function changeSlide(index) {
+            itemsCus[current].classList.remove('endow__container-row-item-active');
+            pagiItems[current].classList.remove('endow__container-pagi-itemMB-active');
+
+            current = index;
+
+            itemsCus[current].classList.add('endow__container-row-item-active');
+            pagiItems[current].classList.add('endow__container-pagi-itemMB-active');
+        }
+
+        // Gắn sự kiện click cho các nút phân trang
+        pagiItems.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                changeSlide(index);
+            });
+        });
+
+        // Khởi động với slide đầu tiên
+        changeSlide(0);
+
+        setInterval(() => {
+            let nextIndex = (current + 1) % itemsCus.length;
+            changeSlide(nextIndex);
+        }, 5000);
+    </script>
+
+    <script>
+        function showToast(message, type = 'info') {
+            const toastContainer = document.getElementById('toast-container');
+            if (!toastContainer) return;
+            // Create toast element
+            const toast = document.createElement('div');
+            toast.classList.add('toast', type);
+            toast.innerHTML = ` ${message}`;
+            // Append toast to container
+            toastContainer.appendChild(toast);
+            // Remove toast after 3 seconds
+            setTimeout(() => {
+                toast.remove();
+            }, 3000);
+        }
+        document.addEventListener('DOMContentLoaded', () => {
+            // Lấy tất cả các form
+            const forms = document.querySelectorAll('.consultation__container-row-left-body');
+
+            // Duyệt qua từng form
+            forms.forEach((form) => {
+                const nameInput = form.querySelector('input[type="text"]');
+                const phoneInput = form.querySelector('input[type="number"]');
+                const submitButton = form.querySelector('.consultation__container-row-left-body-button img');
+
+                // Gắn sự kiện click cho nút trong từng form
+                submitButton.addEventListener('click', () => {
+                    const nameValue = nameInput.value.trim();
+                    const phoneValue = phoneInput.value.trim();
+                    const url = new URL(window.location.toLocaleString());
+                    if (!nameValue || !phoneValue) {
+                        showToast("Vui lòng điền đầy đủ thông tin trong form này!", 'warning');
+                        return;
+                    }
+
+                    let formData = {
+                        sdt: phoneValue,
+                        hoten: nameValue,
+                        url: url.href
+                    };
+
+                    if (formData.sdt !== '') {
+                        const xhr = new XMLHttpRequest();
+                        xhr.open("POST", "<?php echo $local ?>/api/khach-hang/create-user.php", true); // PHP local variable
+                        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState === 4 && xhr.status === 200) {
+                                let response = JSON.parse(xhr.responseText);
+                                if (response.status === 'success') {
+                                    showToast(response.message, 'success');
+                                    // Reset giá trị đúng cách
+                                    phoneValue.value = '';
+                                    nameValue.value = '';
+                                } else {
+                                    showToast(response.message, 'error');
+                                }
+                            }
+                        };
+
+                        xhr.send(JSON.stringify(formData));
+                    } else {
+                        showToast('Số điện thoại không được bỏ trống!', 'warning');
+                    }
+
+
+                });
+            });
+        });
+    </script>
+    <?php include_once "inc/footer.php" ?>
