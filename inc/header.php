8a1cec35b1390b7a6f6b79c491a0ac5da8ae585d
trannhon70 <kevintran351996@gmail.com>
2025-09-10 10:43:48 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -9,7 +9,8 @@ header("Cache-Control: public, max-age=31536000, must-revalidate");
 // Danh sách IP bị chặn
 $blocked_ips = ['115.78.128.131'];
 
-function getClientIp() {
+function getClientIp()
+{
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         return $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -41,25 +42,25 @@ setcookie(
 );
 
 
-    include_once 'classes/bai_viet.php';
-    include_once 'classes/benh.php';
+include_once 'classes/bai_viet.php';
+include_once 'classes/benh.php';
 
-    spl_autoload_register(function ($className) {
-        include_once "classes/" . $className . ".php";
-    });
-    $dbReadStarTime = hrtime(true);
+spl_autoload_register(function ($className) {
+    include_once "classes/" . $className . ".php";
+});
+$dbReadStarTime = hrtime(true);
 
-    $bai_viet = new post();
-    $benh = new Benh();
+$bai_viet = new post();
+$benh = new Benh();
 
-    $dbReadEndTime = hrtime(true);
-    $dbReadTotalTime = ($dbReadEndTime - $dbReadStarTime) / 1e+6;
-   
-    header('Server-Timing: db;desc="Database";dur=' . $dbReadTotalTime);
+$dbReadEndTime = hrtime(true);
+$dbReadTotalTime = ($dbReadEndTime - $dbReadStarTime) / 1e+6;
 
-    // $local ='http://localhost/_andong/phongkhamchuyenkhoaandong.vn'
-    $local ='https://phongkhamchuyenkhoaandong.vn'
-    ?>
+header('Server-Timing: db;desc="Database";dur=' . $dbReadTotalTime);
+
+$local = 'http://localhost/_andong/phongkhamchuyenkhoaandong.vn'
+// $local ='https://phongkhamchuyenkhoaandong.vn'
+?>
 <!DOCTYPE html>
 <html ⚡ lang="en">
 
@@ -86,69 +87,69 @@ setcookie(
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
     <script>
-    function updateHeaderStylesheet() {
-        // Xóa các stylesheet cũ nếu có
-        const existingMobile = document.querySelectorAll('link[id^="mobile-"]');
-        const existingDesktop = document.querySelectorAll('link[id^="desktop-"]');
-        existingMobile.forEach(mobile => mobile.remove());
-        existingDesktop.forEach(desktop => desktop.remove());
+        function updateHeaderStylesheet() {
+            // Xóa các stylesheet cũ nếu có
+            const existingMobile = document.querySelectorAll('link[id^="mobile-"]');
+            const existingDesktop = document.querySelectorAll('link[id^="desktop-"]');
+            existingMobile.forEach(mobile => mobile.remove());
+            existingDesktop.forEach(desktop => desktop.remove());
 
-        // Thêm stylesheet mới dựa trên kích thước cửa sổ
-        if (window.innerWidth < 999) {
-            const mobileLink = [{
-                    href: 'css/header_mobile.min.css',
-                    id: 'mobile-0'
-                },
-                // {
-                //     href: 'css/trang_chu_mobile.min.css',
-                //     id: 'mobile-1'
-                // },
-                {
-                    href: 'css/footer.min.css',
-                    id: 'mobile-1'
-                },
+            // Thêm stylesheet mới dựa trên kích thước cửa sổ
+            if (window.innerWidth < 999) {
+                const mobileLink = [{
+                        href: 'css/header_mobile.min.css',
+                        id: 'mobile-0'
+                    },
+                    // {
+                    //     href: 'css/trang_chu_mobile.min.css',
+                    //     id: 'mobile-1'
+                    // },
+                    {
+                        href: 'css/footer.min.css',
+                        id: 'mobile-1'
+                    },
 
-            ];
-            mobileLink.forEach(({
-                href,
-                id
-            }) => {
-                const link = document.createElement('link');
-                link.rel = 'preload';
-                link.href = href;
-                link.id = id;
-                link.as = 'style';
-                link.onload = function() {
-                    this.rel = 'stylesheet'; // Khi preload xong, đổi sang stylesheet
-                };
-                document.head.appendChild(link);
-            });
+                ];
+                mobileLink.forEach(({
+                    href,
+                    id
+                }) => {
+                    const link = document.createElement('link');
+                    link.rel = 'preload';
+                    link.href = href;
+                    link.id = id;
+                    link.as = 'style';
+                    link.onload = function() {
+                        this.rel = 'stylesheet'; // Khi preload xong, đổi sang stylesheet
+                    };
+                    document.head.appendChild(link);
+                });
 
-        } else {
-            const desktopLink = [{
-                    href: 'css/header.min.css',
-                    id: 'desktop-0'
-                },
-                {
-                    href: 'css/footer.min.css',
-                    id: 'desktop-1'
-                },
+            } else {
+                const desktopLink = [{
+                        href: 'css/header.min.css',
+                        id: 'desktop-0'
+                    },
+                    {
+                        href: 'css/footer.min.css',
+                        id: 'desktop-1'
+                    },
 
-            ];
-            desktopLink.forEach(({
-                href,
-                id
-            }) => {
-                const link = document.createElement('link');
-                link.rel = 'stylesheet';
-                link.href = href;
-                link.id = id;
-                document.head.appendChild(link);
-            });
+                ];
+                desktopLink.forEach(({
+                    href,
+                    id
+                }) => {
+                    const link = document.createElement('link');
+                    link.rel = 'stylesheet';
+                    link.href = href;
+                    link.id = id;
+                    document.head.appendChild(link);
+                });
+            }
         }
-    }
 
-    updateHeaderStylesheet();
+        updateHeaderStylesheet();
     </script>
     <!-- Google tag (gtag.js) -->
     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-QZH0L8X1ZR"></script>
@@ -164,26 +165,26 @@ setcookie(
     </script> -->
 
     <script>
-    // Chỉ tải Google Analytics khi người dùng cuộn xuống
-    document.addEventListener('scroll', function loadGA() {
-        console.log('Người dùng cuộn xuống - Tải Google Analytics');
-        // Tạo thẻ script
-        var g = document.createElement('script'),
-            s = document.scripts[0];
-        g.src = 'https://www.googletagmanager.com/gtag/js?id=G-QZH0L8X1ZR';
-        g.async = true;
-        s.parentNode.insertBefore(g, s);
-        // Cấu hình gtag
-        g.onload = function() {
-            window.dataLayer = window.dataLayer || [];
+        // Chỉ tải Google Analytics khi người dùng cuộn xuống
+        document.addEventListener('scroll', function loadGA() {
+            console.log('Người dùng cuộn xuống - Tải Google Analytics');
+            // Tạo thẻ script
+            var g = document.createElement('script'),
+                s = document.scripts[0];
+            g.src = 'https://www.googletagmanager.com/gtag/js?id=G-QZH0L8X1ZR';
+            g.async = true;
+            s.parentNode.insertBefore(g, s);
+            // Cấu hình gtag
+            g.onload = function() {
+                window.dataLayer = window.dataLayer || [];
 
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'G-QZH0L8X1ZR');
-        };
-        // Xóa sự kiện lắng nghe để không tải lại
-        document.removeEventListener('scroll', loadGA);
-    });
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'G-QZH0L8X1ZR');
+            };
+            // Xóa sự kiện lắng nghe để không tải lại
+            document.removeEventListener('scroll', loadGA);
+        });
     </script>
